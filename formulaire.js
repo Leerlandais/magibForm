5392ba01d3ec409e9fbcfd621586a174312dbca8
Leerlandais <leerlandais@gmail.com>
2024-02-10 21:31:05 +0100

    had another idea, made it better again

--- formulaire.js
+++ formulaire.js
@@ -1,15 +1,16 @@
 const myForm = document.getElementById("myForm");
-const goodL = document.getElementById("goodL");
-const goodC = document.getElementById("goodC");
-const goodN = document.getElementById("goodN");
-const goodS = document.getElementById("goodS");
-const goodA = document.getElementById("goodA");
+const goodLet = document.getElementById("goodLet");
+const goodCap = document.getElementById("goodCap");
+const goodNum = document.getElementById("goodNum");
+const goodSym = document.getElementById("goodSym");
 const nom = document.getElementById("nom");
 const prenom = document.getElementById("prenom");
 const naissance = document.getElementById("naissance");
 const subButton = document.getElementById("subButton");
 const togglePassword = document.querySelector("#togglePassword");
 
+
+function doThisNow(){
 myForm.addEventListener('submit', function(event) {
     event.preventDefault();
     var password = document.getElementById("password").value;
@@ -24,42 +25,34 @@ myForm.addEventListener('submit', function(event) {
         goodA.style.display = "initial";
      return;   
     }else
-        if(password.length < 8){
-            goodL.style.display = "initial";
-            goodC.style.display = "none";
-            goodN.style.display = "none";
-            goodS.style.display = "none";
-        }else if(/[A-Z]/.test(password) === false) {
-            goodC.style.display = "initial";
-            goodL.style.display = "none";
-            goodN.style.display = "none";
-            goodS.style.display = "none";
-        }else if(/\d/.test(password) === false) {
-            goodN.style.display = "initial";
-            goodC.style.display = "none";
-            goodL.style.display = "none";
-            goodS.style.display = "none";
-        }else if (/[!@#$%^,&;*()_+]/.test(password) === false) {
-            goodS.style.display = "initial";
-            goodC.style.display = "none";
-            goodN.style.display = "none";
-            goodL.style.display = "none";
-        }else {
+        if(password.length >= 8){
+            goodLet.style.color = "green";
+        }
+        if(/[A-Z]/.test(password) === true) {
+            goodCap.style.color = "green";
+        }
+        if(/\d/.test(password) === true) {
+            goodNum.style.color = "green";
+        }
+        if (/[!@#$%^,&;*()_+]/.test(password) === true) {
+            goodSym.style.color = "green";
+        }
+        if (password.length >= 8 && /[A-Z]/.test(password) && /\d/.test(password) && ((/[!@#$%^,&;*()_+]/.test(password)))) {
             document.querySelector("body").style.color = "green";
-            goodL.style.display = "none";
-            goodC.style.display = "none";
-            goodN.style.display = "none";
-            goodS.style.display = "none";
             setTimeout(function () {
                 window.location.href = "https://2023.webdev-cf2m.be/Lee/Site/";
             }, 1200);
+            
         }
+    });
+}
+    togglePassword.addEventListener("click", function () {
+        const type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
+        const butType = togglePassword.textContent === "Show" ? "Hide" : "Show";
+        togglePassword.textContent = butType;
+    });
 
-});
 
-togglePassword.addEventListener("click", function () {
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
-    const butType = togglePassword.textContent === "Show" ? "Hide" : "Show";
-    togglePassword.textContent = butType;
-});
+
+// add red and green colours to spans
