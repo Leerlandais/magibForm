c729a581567a3b5e0bac680b3bd9d8919449641e
Leerlandais <leerlandais@gmail.com>
2024-02-10 09:49:22 +0100

    came back to fix date

--- formulaire.js
+++ formulaire.js
@@ -15,23 +15,23 @@ myForm.addEventListener('submit', function(event) {
     var testDate = naissance.value;
     var dateToTest = testDate.substring(0, 4);
     console.log (testDate, dateToTest);
-    if (parseInt(dateToTest) > 2006) {
+    if (parseInt(dateToTest) > 2006 || testDate === ""){
         goodA.style.opacity = 1;
+     return;   
     }
-
         if(password.length < 8){
             goodL.style.opacity = 1;
-            }else if(/[A-Z]/.test(password) === false) {
-                goodC.style.opacity = 1;
-            }else if(/\d/.test(password) === false) {
-                goodN.style.opacity = 1;
-            }else if (/[!@#$%^,&;*()_+]/.test(password) === false) {
-                goodS.style.opacity = 1;
-            }else {
-                document.querySelector("body").style.color = "green";
-                setTimeout(function () {
-                    window.location.href = "https://2023.webdev-cf2m.be/Lee/Site/";
-                }, 1200);
+        }else if(/[A-Z]/.test(password) === false) {
+            goodC.style.opacity = 1;
+        }else if(/\d/.test(password) === false) {
+            goodN.style.opacity = 1;
+        }else if (/[!@#$%^,&;*()_+]/.test(password) === false) {
+            goodS.style.opacity = 1;
+        }else {
+            document.querySelector("body").style.color = "green";
+            setTimeout(function () {
+                window.location.href = "https://2023.webdev-cf2m.be/Lee/Site/";
+            }, 1200);
         }
 
 });
