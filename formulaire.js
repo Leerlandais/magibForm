9d09de87ff43db1d7164238897cc4a81d20d4ef6
Leerlandais <leerlandais@gmail.com>
2024-02-10 08:49:59 +0100

    works but needs to be refreshed

--- formulaire.js
+++ formulaire.js
@@ -3,15 +3,37 @@ const goodL = document.getElementById("goodL");
 const goodC = document.getElementById("goodC");
 const goodN = document.getElementById("goodN");
 const goodS = document.getElementById("goodS");
+const goodA = document.getElementById("goodA");
 const nom = document.getElementById("nom");
 const prenom = document.getElementById("prenom");
 const naissance = document.getElementById("naissance");
 const password = document.getElementById("password").value;
 const subButton = document.getElementById("subButton");
 
-myForm.addEventListener('submit', (event) =>{
-event.preventDefault();
-console.log(password);
+myForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var testDate = naissance.value;
+    var dateToTest = testDate.substring(0, 4);
+    console.log (testDate, dateToTest);
+    if (parseInt(dateToTest) > 2006) {
+        goodA.style.opacity = 1;
+    }
+
+        if(password.length < 8){
+            goodL.style.opacity = 1;
+            }else if(/[A-Z]/.test(password) === false) {
+                goodC.style.opacity = 1;
+            }else if(/\d/.test(password) === false) {
+                goodN.style.opacity = 1;
+            }else if (/[!@#$%^,&;*()_+]/.test(password) === false) {
+                goodS.style.opacity = 1;
+            }else {
+                document.querySelector("body").style.color = "green";
+                setTimeout(function () {
+                    window.location.href = "https://2023.webdev-cf2m.be/Lee/Site/";
+                }, 1200);
+        }
+
 });
 
 
